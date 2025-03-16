33d9a77a9f61b4687a01ad01b5c3decf6606bf19
CatGotBot <kseniiakopylova3@gmail.com>
2025-03-16 11:52:19 -0400

    added a function to retirive a single resort, update resort, delete resort, or create a new resort

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,10 @@ const path = require('path');
 const path = require('pg');
 const client  = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_skiresorts_db');
 
+
 app.use(express.json());
 app.use(require('morgan')('dev'));
+
 
 app.get('/api/resorts', async(req, res, next)=> {
     try {
@@ -20,3 +22,62 @@ app.get('/api/resorts', async(req, res, next)=> {
     }
   });
 
+
+  app.get('/api/resorts/:id', async(req, res, next)=> {
+    try {
+      const SQL = `
+        SELECT * from resorts WHERE id = $1;
+      `;
+      const response = await client.query(SQL, [ req.params.id]);
+      res.send(response.rows[0]);
+    }
+    catch(ex){
+      next(ex);
+    }
+  });
+  
+  app.put('/api/resorts/:id', async(req, res, next)=> {
+    try {
+      const SQL = `
+        UPDATE resorts
+        SET name=$1, is_location=$2, updated_at= now()
+        WHERE id=$3 RETURNING *
+      `;
+      const response = await client.query(SQL, [ req.body.name, req.body.is_location, req.params.id]);
+      res.send(response.rows[0]);
+    }
+    catch(ex){
+      next(ex);
+    }
+  });
+  
+
+  app.delete('/api/resorts/:id', async(req, res, next)=> {
+    try {
+      const SQL = `
+        DELETE from resorts
+        WHERE id = $1
+      `;
+      const response = await client.query(SQL, [ req.params.id]);
+      res.sendStatus(204);
+    }
+    catch(ex){
+      next(ex);
+    }
+  });
+  
+  app.post('/api/resorts', async(req, res, next)=> {
+    try {
+      const SQL = `
+        INSERT INTO resorts(name, is_location)
+        VALUES($1, $2)
+        RETURNING *
+      `;
+      const response = await client.query(SQL, [ req.body.name, req.body.is_location]);
+      res.send(response.rows[0]);
+    }
+    catch(ex){
+      next(ex);
+    }
+  });
+  
