d3785efc433c2ccef1b5892d48c1fecb491ab9b3
CatGotBot <kseniiakopylova3@gmail.com>
2025-03-16 11:37:14 -0400

    imported reqired modules, made a database connection, added midlware configuration, added api endpoints

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,19 @@ const path = require('path');
 const path = require('pg');
 const client  = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_skiresorts_db');
 
+app.use(express.json());
+app.use(require('morgan')('dev'));
 
-
+app.get('/api/resorts', async(req, res, next)=> {
+    try {
+      const SQL = `
+        SELECT * from resorts ORDER BY created_at DESC;
+      `;
+      const response = await client.query(SQL);
+      res.send(response.rows);
+    }
+    catch(ex){
+      next(ex);
+    }
+  });
 
